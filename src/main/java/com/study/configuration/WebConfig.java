0f6a7d38691beca927eef65498e0799a7eb24220
Kapelushny <zig.tos@gmail.com>
2021-12-13 16:06:01 +0200

    [b-2] Get 3 random movies  - fix application.properties source values

--- src/main/java/com/study/configuration/WebConfig.java
+++ src/main/java/com/study/configuration/WebConfig.java
@@ -8,13 +8,13 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@Configuration
-//@PropertySource(value={"classpath:static/application.yml"})
+//@PropertySource(value={"classpath:static/application.properties"})
 //class WebConfig implements WebMvcConfigurer {
 //
 //    @Bean
 //    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
 //        PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
-//        pspc.setLocation(new ClassPathResource("target/classes/application.yml"));
+//        pspc.setLocation(new ClassPathResource("target/classes/application.properties"));
 //        return pspc;
 //    }
 //}

44a84efb68ebddb8a97cb7f76965351228bf899a
Kapelushny <zig.tos@gmail.com>
2021-12-16 02:28:20 +0200

    [t-1] Create genre cache

--- src/main/java/com/study/configuration/RootConfiguration.java
+++ src/main/java/com/study/configuration/RootConfiguration.java
@@ -3,9 +3,11 @@ package com.study.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @ComponentScan({"com.study.service", "com.study.repository"})
 @Import(JdbcConfig.class)
+@EnableScheduling
 public class RootConfiguration {
 }

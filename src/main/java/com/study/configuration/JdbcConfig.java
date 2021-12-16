790316f3d68e2a224f42eac8de654c626eb2c5db
Kapelushny <zig.tos@gmail.com>
2021-12-16 10:21:48 +0200

    [t-1] add hikari settings

--- src/main/java/com/study/configuration/JdbcConfig.java
+++ src/main/java/com/study/configuration/JdbcConfig.java
@@ -19,13 +19,20 @@ public class JdbcConfig {
                                  @Value("${db.url}") String url,
                                  @Value("${db.username}") String username,
                                  @Value("${db.password}") String password,
-                                 @Value("${db.maximumPoolSize}") int maximumPoolSize) {
+                                 @Value("${db.maximumPoolSize:3}") int maximumPoolSize,
+                                 @Value("${db.connectionTimout:30000}") int connectionTimout,
+                                 @Value("${db.idleTimeout:30000}") int idleTimeout,
+                                 @Value("${db.maxLifetime:1800000}") int maxLifetime
+    ) {
         HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setDriverClassName(driver);
         hikariConfig.setJdbcUrl(url);
         hikariConfig.setUsername(username);
         hikariConfig.setPassword(password);
         hikariConfig.setMaximumPoolSize(maximumPoolSize);
+        hikariConfig.setConnectionTimeout(connectionTimout);
+        hikariConfig.setIdleTimeout(idleTimeout);
+        hikariConfig.setMaxLifetime(maxLifetime);
 
         return new HikariDataSource(hikariConfig);
     }
